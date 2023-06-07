6387f1664ac16b319c7ebf73c7567fb31d628eaf
edueevaristo <eduevaristo_dev@hotmail.com>
2023-06-07 04:42:40 -0300

    Commit Alteração | Considerações Finais

--- js/index.js
+++ js/index.js
@@ -1,5 +1,42 @@
-
+let tipo1 = false;
+let tipo2 = false;
+let horariosDia = ['09:00', '10:00', '11:00', '14:00', '15:00', '16:00'];
 let currentStep = 1;
+
+$(document).ready(function () {
+
+  showStep(1);
+  gerarHorarios();
+  $('#step-1').addClass('active');
+
+  /* Validando se o input de checkbox foi clicado */
+  $('#tipo-agendamento-1').change(function () {
+    if ($(this).is(':checked')) {
+      tipo1 = true;
+    } else {
+      tipo1 = false;
+    }
+  });
+
+  $('#tipo-agendamento-2').change(function () {
+    if ($(this).is(':checked')) {
+      tipo2 = true;
+    } else {
+      tipo2 = false;
+    }
+  });
+
+  $('#step-1-next').click(function () {
+    validar = validarTipos();
+
+    if (validar == false) {
+      return false;
+    } else {
+      nextStep();
+    }
+  });
+
+});
 
 function showStep(step) {
   $(`#step${step}`).show();
@@ -21,14 +58,12 @@ function nextStep() {
     $('#step-4').removeClass('active').addClass('disabled');
 
   } else if (currentStep === 2) {
-
     $('#step-1').removeClass('active');
     $('#step-2').addClass('active');
     $('#step-3').removeClass('active').addClass('disabled');
     $('#step-4').removeClass('active').addClass('disabled');
 
   } else if (currentStep === 3) {
-
     $('#step-1').removeClass('active');
     $('#step-2').removeClass('active');
     $('#step-3').addClass('active');
@@ -44,21 +79,18 @@ function prevStep() {
   showStep(currentStep);
 
   if (currentStep === 1) {
-
     $('#step-1').addClass('active');
     $('#step-2').removeClass('active').addClass('disabled');
     $('#step-3').removeClass('active').addClass('disabled');
     $('#step-4').removeClass('active').addClass('disabled');
 
   } else if (currentStep === 2) {
-
     $('#step-1').removeClass('active');
     $('#step-2').addClass('active');
     $('#step-3').removeClass('active').addClass('disabled');
     $('#step-4').removeClass('active').addClass('disabled');
 
   } else if (currentStep === 3) {
-
     $('#step-1').removeClass('active');
     $('#step-2').removeClass('active');
     $('#step-3').addClass('active');
@@ -67,6 +99,21 @@ function prevStep() {
   }
 }
 
-showStep(1);
+function validarTipos() {
+  if (tipo1 && tipo2) {
+    alert("Há dois tipos de atendimento selecionados. Selecione apenas um deles para prosseguir.")
+    return false;
+  }
+}
 
-$('#step-1').addClass('active');
+function gerarHorarios() {
+  var divHorarios = $('#horarios-disponiveis');
+  divHorarios.empty(); // Limpa os horários existentes antes de gerar novamente
+
+  for (var i = 0; i < horariosDia.length; i++) {
+    var horario = horariosDia[i];
+    var checkbox = $('<input type="checkbox" value="' + horario + '" class="form-check-input style-tipo-agendamento-2">');
+    var label = $('<label class="form-check-label text-size-agendamento-2 mr-3">').text(horario);
+    divHorarios.append($('<div class="form-check">').append(checkbox).append(label));
+  }
+}
