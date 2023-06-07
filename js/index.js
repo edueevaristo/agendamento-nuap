8f42f48f002bfa298449de032e549823b0b08afe
edueevaristo <eduevaristo_dev@hotmail.com>
2023-06-07 05:37:22 -0300

    Commit Implementações | Considerações Finais

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 let tipo1 = false;
 let tipo2 = false;
 let horariosDia = ['09:00', '10:00', '11:00', '14:00', '15:00', '16:00'];
-let currentStep = 1;
+let passoAtual = 1;
 
 $(document).ready(function () {
 
-  showStep(1);
+  mostrarPasso(1);
   gerarHorarios();
   $('#step-1').addClass('active');
 
@@ -32,38 +32,47 @@ $(document).ready(function () {
     if (validar == false) {
       return false;
     } else {
-      nextStep();
+      proximoPasso();
     }
   });
 
+  var valores = [];
+  $('.select-analista input[type="checkbox"]').change(function () {
+    $('.select-analista input[type="checkbox"]:checked').each(function () {
+      var valor = $(this).next('label').text().trim();
+      valores.push(valor);
+    });
+  })
+
 });
 
-function showStep(step) {
+
+function mostrarPasso(step) {
   $(`#step${step}`).show();
 }
 
-function hideStep(step) {
+function esconderPasso(step) {
   $(`#step${step}`).hide();
 }
 
-function nextStep() {
-  hideStep(currentStep);
-  currentStep++;
-  showStep(currentStep);
+function proximoPasso() {
+  esconderPasso(passoAtual);
+  passoAtual++;
+  mostrarPasso(passoAtual);
 
-  if (currentStep === 1) {
+  if (passoAtual === 1) {
     $('#step-1').addClass('active');
     $('#step-2').removeClass('active').addClass('disabled');
     $('#step-3').removeClass('active').addClass('disabled');
     $('#step-4').removeClass('active').addClass('disabled');
 
-  } else if (currentStep === 2) {
+  } else if (passoAtual === 2) {
     $('#step-1').removeClass('active');
     $('#step-2').addClass('active');
     $('#step-3').removeClass('active').addClass('disabled');
     $('#step-4').removeClass('active').addClass('disabled');
 
-  } else if (currentStep === 3) {
+  } else if (passoAtual === 3) {
     $('#step-1').removeClass('active');
     $('#step-2').removeClass('active');
     $('#step-3').addClass('active');
@@ -72,25 +81,25 @@ function nextStep() {
   }
 }
 
-function prevStep() {
+function voltarPasso() {
 
-  hideStep(currentStep);
-  currentStep--;
-  showStep(currentStep);
+  esconderPasso(passoAtual);
+  passoAtual--;
+  mostrarPasso(passoAtual);
 
-  if (currentStep === 1) {
+  if (passoAtual === 1) {
     $('#step-1').addClass('active');
     $('#step-2').removeClass('active').addClass('disabled');
     $('#step-3').removeClass('active').addClass('disabled');
     $('#step-4').removeClass('active').addClass('disabled');
 
-  } else if (currentStep === 2) {
+  } else if (passoAtual === 2) {
     $('#step-1').removeClass('active');
     $('#step-2').addClass('active');
     $('#step-3').removeClass('active').addClass('disabled');
     $('#step-4').removeClass('active').addClass('disabled');
 
-  } else if (currentStep === 3) {
+  } else if (passoAtual === 3) {
     $('#step-1').removeClass('active');
     $('#step-2').removeClass('active');
     $('#step-3').addClass('active');
@@ -116,4 +125,46 @@ function gerarHorarios() {
     var label = $('<label class="form-check-label text-size-agendamento-2 mr-3">').text(horario);
     divHorarios.append($('<div class="form-check">').append(checkbox).append(label));
   }
+}
+
+function finalizarAgendamento() {
+  const swalWithBootstrapButtons = Swal.mixin({
+    buttonsStyling: true
+  });
+  
+  swalWithBootstrapButtons.fire({
+    title: 'Você tem certeza?',
+    text: "A ação não poderá ser revertida!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Sim, prosseguir com o agendamento.',
+    cancelButtonText: 'Não, cancele.',
+    reverseButtons: true,
+    customClass: {
+      confirmButton: 'swal-button-spacing',
+      cancelButton: 'swal-button-spacing'
+    },
+    buttonsStyling: false,
+    confirmButtonClass: 'btn btn-success',
+    cancelButtonClass: 'btn btn-danger'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      swalWithBootstrapButtons.fire(
+        'Agendamento realizado com sucesso!',
+        'O seu agendamento foi concluído, em breve, entraremos em contato com você para mais informações!',
+        'success'
+      );
+    } else if (result.dismiss === Swal.DismissReason.cancel) {
+      swalWithBootstrapButtons.fire(
+        'Cancelado com sucesso!',
+        'Salvamos em algum lugar de sua memória mental :)',
+        'error'
+      );
+      
+    }
+  });
+  setInterval(() => {
+    location.reload();
+  }, 6900);
+  
 }
